f885217f87a7342311163d9b9c4093a0abf4ec19
kobbe.vereecken <Kobbe12@hotmail.com>
2022-04-14 18:09:41 +0200

    Extra tweaking

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,5 @@
 'use strict';
 
-function navBarScrollingChanges() {
-    const header = document.querySelector('.header.container');
-    document.addEventListener('scroll', () => {
-        const scroll_position = window.scrollY;
-        if (scroll_position > 250) {
-            header.style.backgroundColor = '#000000';
-        } else {
-            header.style.backgroundColor = 'transparent';
-        }
-    });
-}
-
 function toggleActive() {
     const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
     const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
--- assets/js/init.js
+++ assets/js/init.js
@@ -23,3 +23,15 @@ function initStats(){
         navOptions.forEach(options => options.addEventListener('click', selectedOption));
     }
 }
+
+function navBarScrollingChanges() {
+    const header = document.querySelector('.header.container');
+    document.addEventListener('scroll', () => {
+        const scroll_position = window.scrollY;
+        if (scroll_position > 250) {
+            header.style.backgroundColor = '#000000';
+        } else {
+            header.style.backgroundColor = 'transparent';
+        }
+    });
+}
