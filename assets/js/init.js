f23f956a41a5cb479dfc052e62eb1060bdec51b7
kobbe.vereecken <Kobbe12@hotmail.com>
2022-04-14 17:37:06 +0200

    fully build static stats site

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,8 +1,23 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded',init);
+document.addEventListener('DOMContentLoaded',initIndex);
+document.addEventListener('DOMContentLoaded',initStats);
 
-function init(){
+function initIndex(){
+    const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+	const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+	if(hamburger){
+		hamburger.addEventListener('click', toggleActive);
+	}
+	if(menu_item){
+		menu_item.forEach((item) => {
+			item.addEventListener('click', toggleActive);
+		});
+	}
+	navBarScrollingChanges();
+}
+
+function initStats(){
     if(document.querySelectorAll('.navMenu a')) {
         const navOptions = document.querySelectorAll('.navMenu a');
         navOptions.forEach(options => options.addEventListener('click', selectedOption));
--- assets/js/stats.js
+++ assets/js/stats.js
@@ -1,6 +1,8 @@
 'use strict';
 
 function selectedOption(e) {
+    document.querySelector('#card-data').classList.remove('hide');
+    document.querySelector('#information').classList.add('hide');
     const navOptions = e.target.closest('a').innerHTML;
     document.querySelector('#api-data h2').innerHTML = `Your <span>${navOptions}</span>`;
 }
